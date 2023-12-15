667394384fed3b2def6f72844c8edf4d841f85cf
aradhya-10 <202051034@iiitvadodara.ac.in>
2023-12-15 14:17:08 +0530

    UC7: Size by City and State

--- src/msSqlQueries.sql
+++ src/msSqlQueries.sql
@@ -124,3 +124,46 @@ mysql> SELECT * FROM AddressBook WHERE city='Gwalior' OR state='Gujarat';
 | Garima     | Mangal    | Bada     | Gwalior     | MP      | 474009 | +91 6394772288 | mangal.garima@gmail.com      |
 +------------+-----------+----------+-------------+---------+--------+----------------+------------------------------+
 2 rows in set (0.00 sec)
+
+
+mysql> INSERT INTO AddressBook (first_name, last_name, address, city, state, zip
+    -> , phone_number, email) VALUES ('Riya', 'Mishra', 'Noida', 'Gaziabad', 'UP', '382420', '+91 6394972187', 'riya.mishra7800@gmail.com');
+Query OK, 1 row affected (0.01 sec)
+
+mysql> SELECT * FROM AddressBook;
++------------+-----------+----------+-------------+---------+--------+----------------+------------------------------+
+| first_name | last_name | address  | city        | state   | zip    | phone_number   | email                        |
++------------+-----------+----------+-------------+---------+--------+----------------+------------------------------+
+| Aradhya    | Mishra    | Sargasan | Gandhinagar | Gujarat | 382010 | +91 6394972188 | aradhya.mishra1012@gmail.com |
+| Garima     | Mangal    | Bada     | Gwalior     | MP      | 474009 | +91 6394772288 | mangal.garima@gmail.com      |
+| Riya       | Mishra    | Noida    | Gaziabad    | UP      | 382420 | +91 6394972187 | riya.mishra7800@gmail.com    |
+| Sanjana    | Pathak    | Shahpur  | Gorakhpur   | UP      | 273014 | +91 6394972288 | sanjana.pathak@gmail.com     |
++------------+-----------+----------+-------------+---------+--------+----------------+------------------------------+
+4 rows in set (0.00 sec)
+
+mysql> INSERT INTO AddressBook (first_name, last_name, address, city, state, zip, phone_number, email) VALUES ('Naman', 'Mishra', 'Infocity', 'Gandhinagar', 'Gujarat', '382010', '+91 6394972199', 'naman.mishra1012@gmail.com');
+Query OK, 1 row affected (0.02 sec)
+
+mysql> SELECT * FROM AddressBook;
++------------+-----------+----------+-------------+---------+--------+----------------+------------------------------+
+| first_name | last_name | address  | city        | state   | zip    | phone_number   | email                        |
++------------+-----------+----------+-------------+---------+--------+----------------+------------------------------+
+| Aradhya    | Mishra    | Sargasan | Gandhinagar | Gujarat | 382010 | +91 6394972188 | aradhya.mishra1012@gmail.com |
+| Garima     | Mangal    | Bada     | Gwalior     | MP      | 474009 | +91 6394772288 | mangal.garima@gmail.com      |
+| Naman      | Mishra    | Infocity | Gandhinagar | Gujarat | 382010 | +91 6394972199 | naman.mishra1012@gmail.com   |
+| Riya       | Mishra    | Noida    | Gaziabad    | UP      | 382420 | +91 6394972187 | riya.mishra7800@gmail.com    |
+| Sanjana    | Pathak    | Shahpur  | Gorakhpur   | UP      | 273014 | +91 6394972288 | sanjana.pathak@gmail.com     |
++------------+-----------+----------+-------------+---------+--------+----------------+------------------------------+
+5 rows in set (0.00 sec)
+
+-- UC7: Understand the size of address book by City and State
+mysql> SELECT city, state, COUNT(*) AS size FROM AddressBook GROUP BY city, state;
++-------------+---------+------+
+| city        | state   | size |
++-------------+---------+------+
+| Gandhinagar | Gujarat |    2 |
+| Gaziabad    | UP      |    1 |
+| Gorakhpur   | UP      |    1 |
+| Gwalior     | MP      |    1 |
++-------------+---------+------+
+4 rows in set (0.00 sec)
