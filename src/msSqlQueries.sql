77ff4ccfe240f5bd93f918968de545487cc0f458
aradhya-10 <202051034@iiitvadodara.ac.in>
2023-12-15 11:22:58 +0530

    UC1: Create DB

---
+++ src/msSqlQueries.sql
@@ -0,0 +1,14 @@
+mysql> SHOW DATABASES;
++--------------------+
+| Database           |
++--------------------+
+| information_schema |
+| mysql              |
+| performance_schema |
+| test               |
++--------------------+
+4 rows in set (0.00 sec)
+
+-- UC1: Create an Address Book Service DB
+mysql> CREATE DATABASE AddressBookService;
+Query OK, 1 row affected (0.00 sec)
